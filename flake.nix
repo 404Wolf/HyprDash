459ae98f91ec6961a63956d7821d12e60ea92bfb
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-06-20 23:17:32 -0400

    Add executable

--- flake.nix
+++ flake.nix
@@ -6,19 +6,31 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         nodejs = pkgs.nodejs_22;
-      in rec {
-        packages = {
+      in
+      rec {
+        packages = rec {
+          executable = pkgs.writeShellScriptBin "hyprland-dash-to-dock" ''
+            ${nodejs}/bin/node ${default}/bin/hyprland-dask-to-dock.js
+          '';
           default = pkgs.buildNpmPackage {
             name = "hyprland-dask-to-dock";
             src = ./.;
-            npmDepsHash = "sha256-umiVLkD9gQA11vDfzEkdKIbz7n5jZ4MCHql++uz1Azk=";
+            npmDepsHash = "sha256-RuxViAgj2Ev8hVfQWzfRNERdFM/ASBbYkmjy3bigh1c=";
             dontNpmInstall = true;
-            postInstall = '' mv $out/bin/index.js $out/bin/hyprland-dask-to-dock.js '';
+            postInstall = ''
+              mv $out/bin/index.js $out/bin/hyprland-dask-to-dock.js 
+            '';
             meta = {
               description = "Hyprland dashtodock implementation";
               license = "MIT";
@@ -26,11 +38,16 @@
           };
         };
         devShells = {
-          default = pkgs.mkShell { 
-            out=".";
-            packages = [ nodejs pkgs.prefetch-npm-deps pkgs.nodePackages.pnpm ];
+          default = pkgs.mkShell {
+            out = ".";
+            packages = [
+              nodejs
+              pkgs.prefetch-npm-deps
+              pkgs.nodePackages.pnpm
+            ];
             shellHook = ''pnpm install; prefetch-npm-deps package-lock.json'';
           };
         };
-      });
+      }
+    );
 }
