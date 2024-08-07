c0f1bbce47c7d606686d0b61257d7b6415b4d2dc
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-08-07 12:36:54 -0400

    Fix bug where it is unfocused

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,68 @@
-import {ArgumentParser} from 'argparse';
-import {runCommand, hyprctlDispatch, launchTaggedClient, getClientByTag} from './utils';
-import {version} from '../package.json';
+import { ArgumentParser } from "argparse";
+import {
+  activeWorkspace,
+  hyprctlDispatch,
+  launchTaggedClient,
+  getClientByTag,
+} from "./utils";
+import { version } from "../package.json";
 
 const parser = new ArgumentParser({
-  description: 'Hyprctl implementation of Gnome\'s dashtodock extension.',
+  description: "Hyprctl implementation of Gnome's dashtodock extension.",
 });
 
-parser.add_argument('-v', '--version', {action: 'version', version});
-parser.add_argument('-appID', {help: 'The ID to use for naming for the application to make dash-to-dock\'d', required: true});
-parser.add_argument('-launchCommand', {help: 'The command to launch the application', required: true});
+parser.add_argument("-v", "--version", { action: "version", version });
+parser.add_argument("-appID", {
+  help: "The ID to use for naming for the application to make dash-to-dock'd",
+  required: true,
+});
+parser.add_argument("-launchCommand", {
+  help: "The command to launch the application",
+  required: true,
+});
+parser.add_argument("-matcher", {
+  help: "Regex to use to ensure the correct PID is found",
+  default: /\w/,
+  required: false,
+});
 
 const args = parser.parse_args();
+args.matcher = new RegExp(args.matcher);
 
 (async () => {
-  const tag = `ephemeral${args.appID}`
-  const workspace = `ephemeral${args.appID}`
+  const tag = `ephemeral${args.appID}`;
+  const workspace = `ephemeral${args.appID}`;
 
   // General scheme:
   //
-  // Kitty is running
-  //   Kitty in scratchpad workspace
-  //     Move Kitty to current workspace
+  // App is running
+  //   App in scratchpad workspace
+  //     Move App to current workspace
   //   Else
-  //     Move kitty to scratchpad workspace
+  //     Move App to scratchpad workspace
   // Else
-  //   Launch Kitty
+  //   Launch App
 
-  const ephemeralApp = await getClientByTag(tag)
+  const ephemeralApp = await getClientByTag(tag);
   if (ephemeralApp) {
     if (ephemeralApp.workspace.name === workspace) {
-      await hyprctlDispatch(`pin pid:${ephemeralApp.pid}`)
-      await hyprctlDispatch(`focuswindow pid:${ephemeralApp.pid}`)
-      await hyprctlDispatch(`movetoworkspacesilent +0,pid:${ephemeralApp.pid}`)
+      const activeWorkspaceID = (await activeWorkspace()).id;
+      await hyprctlDispatch(`pin pid:${ephemeralApp.pid}`);
+      await hyprctlDispatch(
+        `movetoworkspacesilent ${activeWorkspaceID},pid:${ephemeralApp.pid}`,
+      );
+    } else {
+      await hyprctlDispatch(`pin pid:${ephemeralApp.pid}`);
+      await hyprctlDispatch(
+        `movetoworkspacesilent special:${workspace},pid:${ephemeralApp.pid}`,
+      );
     }
-    else {
-      await hyprctlDispatch(`pin pid:${ephemeralApp.pid}`)
-      await hyprctlDispatch(`movetoworkspacesilent special:${workspace},pid:${ephemeralApp.pid}`)
-    }
+  } else {
+    await launchTaggedClient(
+      args.launchCommand,
+      tag,
+      ["pin", "size 65% 60%", "float", "focus"],
+      args.matcher,
+    );
   }
-  else {
-    await launchTaggedClient(args.launchCommand, tag, ["pin", "size 65% 60%", "float", "focus"])
-  }
-})()
-
+})();
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,21 @@
-import {exec} from "child_process"
-import {Client} from "./types"
+import { exec } from "child_process";
+import { Client } from "./types";
+
+const uuid = () => Math.floor(Math.random() * 90000) + 10000;
 
 export function runCommand(command: string): Promise<string> {
-  console.log(`Running command ${command}`)
+  console.log(`Running command ${command}`);
   return new Promise((resolve) => {
     exec(command, (error, stdout, stderr) => {
-      resolve(stdout)
-    })
-  })
-};
+      resolve(stdout);
+    });
+  });
+}
+
+export async function activeWorkspace() {
+  const workspace = JSON.parse(await runCommand("hyprctl activeWorkspace -j"));
+  return workspace as any;
+}
 
 async function getClients(): Promise<Client[]> {
   return JSON.parse(await runCommand("hyprctl clients -j"));
@@ -16,47 +23,50 @@ async function getClients(): Promise<Client[]> {
 
 async function getClientTags() {
   const clients = await getClients();
-  const clientTags = clients.map((client: any) => client.tags).flat() as string[]
-  console.log(clientTags)
-  return clientTags
+  const clientTags = clients
+    .map((client: any) => client.tags)
+    .flat() as string[];
+  console.log(clientTags);
+  return clientTags;
 }
 
-async function waitUntilTagged(pid: number, tag: string) {
-  console.log(`Waiting for tag ${tag} on pid ${pid}`)
-  let clients = await getClientTags()
-  while (!clients.includes(tag)) {
-    clients = await getClientTags()
-    await runCommand(`hyprctl dispatch tagwindow +${tag} pid:${pid}`)
+export async function launchTaggedClient(
+  launchCommand: string,
+  tag: string,
+  rules: string[] = [],
+  matcher = /\w/,
+) {
+  const creationWorkspaceId = uuid().toString();
+  rules.push(`workspace special:${creationWorkspaceId}`);
+  const stringifiedRules = rules.join(";");
+  await runCommand(
+    `hyprctl "dispatch exec [${stringifiedRules}] ${launchCommand}"`,
+  );
+
+  let tries = 0;
+  while (true) {
+    if (tries > 10) {
+      throw new Error("Could not find the client after 10 tries");
+    }
+    tries++;
+    const clients = await getClients();
+    if (
+      clients.find((client: Client) =>
+        client.workspace.name.includes(creationWorkspaceId),
+      )
+    ) {
+      hyprctlDispatch(`tagwindow ${tag}`);
+    }
   }
 }
 
-export async function launchTaggedClient(launchCommand: string, tag: string, rules: string[] = [], matcher = /\w/) {
-  const stringifiedRules = rules.join(";")
-  await runCommand(`hyprctl "dispatch exec [${stringifiedRules}] ${launchCommand}"`)
-  const pidGetterOutput = (await runCommand("ps aux --sort +start_time | tail -n 4")).split("\n")[0]
-
-  // Raise error if matcher is not found
-  if (!matcher.test(pidGetterOutput)) throw new Error("No PID found for the application.")
-
-  const pids = pidGetterOutput.match(/\d+/g)
-  if (!pids) throw new Error("No PID found for the application.")
-  const pid = pids[0]
-
-  await waitUntilTagged(parseInt(pid), tag)
-
-  return pid
-}
-
 export async function getClientByTag(tag: string): Promise<Client | false> {
-  const clients = await getClients()
-  const client = clients.find((client: any) => client.tags.includes(tag))
-  if (client === undefined)
-    return false
-  return client
+  const clients = await getClients();
+  const client = clients.find((client: any) => client.tags.includes(tag));
+  if (client === undefined) return false;
+  return client;
 }
 
 export async function hyprctlDispatch(command: string) {
-  return await runCommand(`hyprctl dispatch ${command}`)
+  return await runCommand(`hyprctl dispatch ${command}`);
 }
-
-
