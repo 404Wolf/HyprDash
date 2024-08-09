d0716aba61ae674232c4e7c6b87f72d1cd95f51e
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-08-09 18:42:08 -0400

    Update flake

--- flake.nix
+++ flake.nix
@@ -6,22 +6,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        nodejs = pkgs.nodejs_22;
-      in
-      rec {
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
         packages = rec {
           executable = pkgs.writeShellScriptBin "hyprland-dash-to-dock" ''
-            ${nodejs}/bin/node ${default}/bin/hyprland-dask-to-dock.js $@
+            ${pkgs.bun}/bin/bun ${default}/bin/hyprland-dask-to-dock.js $@
           '';
           default = pkgs.buildNpmPackage {
             name = "hyprland-dask-to-dock";
@@ -29,7 +25,7 @@
             npmDepsHash = "sha256-RuxViAgj2Ev8hVfQWzfRNERdFM/ASBbYkmjy3bigh1c=";
             dontNpmInstall = true;
             postInstall = ''
-              mv $out/bin/index.js $out/bin/hyprland-dask-to-dock.js 
+              mv $out/bin/index.js $out/bin/hyprland-dask-to-dock.js
             '';
             meta = {
               description = "Hyprland dashtodock implementation";
@@ -41,9 +37,9 @@
           default = pkgs.mkShell {
             out = ".";
             packages = [
-              nodejs
               pkgs.prefetch-npm-deps
               pkgs.nodePackages.pnpm
+              pkgs.bun
             ];
             shellHook = ''pnpm install; prefetch-npm-deps package-lock.json'';
           };
